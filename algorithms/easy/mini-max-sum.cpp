b7f460d1a971513a11c57e56d3cd84543fcc9604
Cristian Hernández <129994548+crishgit@users.noreply.github.com>
2023-04-28 08:54:57 -0400

    mini-max-sum now work 15/15

--- algorithms/easy/mini-max-sum.cpp
+++ algorithms/easy/mini-max-sum.cpp
@@ -1,24 +1,13 @@
-/*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-// I think the error is in the indexing of the array
-// Have problem with arrays of size <5
-
-int sum_array_elements_range(vector<int> arr, int start, int end) {
-    int long long sumTotal = 0;
-    for (int i = start; i <= end; i++) {
-        sumTotal+= arr[i];
-    }
-    return sumTotal;
-}
-
 void miniMaxSum(vector<int> arr) {
-    int long long minSum = 0; int long long maxSum = 0;
+    int long long unsigned minSum = 0; int long long unsigned maxSum = 0;
     sort(arr.begin(), arr.end());
-    minSum = sum_array_elements_range(arr, 0, arr.size() - 1 - 1);
-    maxSum = sum_array_elements_range(arr, 1, arr.size() - 1);
+
+    for (int i = 0; i <= 3; i++) {
+        minSum+= arr[i];
+    }
+    for (int i = 1; i <= 4; i++) {
+        maxSum+= arr[i];
+    }
+
     cout << minSum << " " << maxSum << endl;
 }
