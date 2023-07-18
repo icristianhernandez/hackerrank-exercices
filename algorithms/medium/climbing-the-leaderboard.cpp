349d6d2bb2fcc238d3db53ec672ae2487ad5c39c
Cristian Hernández <129994548+crishgit@users.noreply.github.com>
2023-07-18 16:45:42 -0400

    added climbing-the-leaderboard.cpp

--- algorithms/medium/climbing-the-leaderboard.cpp
+++ algorithms/medium/climbing-the-leaderboard.cpp
@@ -1,48 +1,53 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+vector<int> noRepeatedNumberOrderedVector(const vector<int> ordered_vector) {
+    vector<int> no_repeated_vector = {};
+
+    int last_number_added = ordered_vector[0];
+    no_repeated_vector.push_back(ordered_vector[0]);
+
+    for(int i = 1; i < ordered_vector.size(); i++) {
+        if(last_number_added > ordered_vector[i] ) {
+            no_repeated_vector.push_back(ordered_vector[i] );
+            last_number_added = ordered_vector[i];
+        }
+    }
+
+    return no_repeated_vector;
+}
+
 vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
-    
-    vector<int> new_player_scores(player.size() );
-    vector<int> ranked_copy = ranked;
-    
-    for (int i = 0; i < player.size(); i++) {
-        int player_score = player[i];
-        int actual_rank = 1;
-        int actual_score = ranked_copy[0];
-        
-        for (int k = 0; k < ranked_copy.size(); k++) {
-            int rank_score = ranked_copy[k];
-            
-            if(k == 0 and (player_score > actual_score) ){
-                new_player_scores[i] = actual_rank;
+    vector<int> new_rankings = {};
+    vector<int> no_repeated_ranked = noRepeatedNumberOrderedVector(ranked);
+    // added 0 for adapt the algorithm to the case that the player record it's 
+    // lower than all the rankeds
+    no_repeated_ranked.push_back(0);
+    int last_index_checked_rank = no_repeated_ranked.size() - 1;
+
+    for(auto player_record : player) {
+        for(int i = last_index_checked_rank; i >= 0; i--) {
+            int ranked_record = no_repeated_ranked[i];
+
+            if(player_record == ranked_record) {
+                new_rankings.push_back(i + 1);
+                last_index_checked_rank = i;
                 break;
             }
-            
-            if (actual_score == player_score ) {
-                new_player_scores[i] = actual_rank;
+            else if(player_record < ranked_record) {
+                new_rankings.push_back(i + 1 + 1);
+                last_index_checked_rank = i + 1;
                 break;
             }
-            else if (rank_score < actual_score) {
-                if(rank_score < player_score) {
-                    actual_rank++;
-                    new_player_scores[i] = actual_rank;
-                    break;
-                }
-                else{
-                    actual_rank++;
-                    actual_score = rank_score;
-                }
-            }
-            if (k == (ranked_copy.size() - 1) ) {
-                if(actual_score == player_score){
-                    actual_rank--;
-                }
-                
-                actual_rank++;
-                new_player_scores[i] = actual_rank;
+            else if(i == 0) {
+                new_rankings.push_back(i+ 1);
+                last_index_checked_rank = i ;
                 break;
             }
         }
     }
 
-
-    return new_player_scores;
+    return new_rankings;
 }
+
