8c2f1f39e586356f853b02ee765a8320782f3314
Cristian Hernández <129994548+crishgit@users.noreply.github.com>
2023-07-17 21:35:45 -0400

    improved version of electronics shop

--- algorithms/easy/electronics-shop.cpp
+++ algorithms/easy/electronics-shop.cpp
@@ -1,65 +1,62 @@
 /*
-Manual Test to:
+most expensive pair of keyboard and drive that a person can buy
+*/ 
+#include <bits/stdc++.h>
+using namespace std;
+
+// I are going to assume that the most expensive pair is one of two: 
+// a) the most expensive keyboard who can allow to buy the least expensive drive 
+// b) viceversa
+
+/* 
+error:
 10 2 3
 3 1
 5 2 8
 
-Expected 9
+Expected 9 but return  -1
+9 it's: 1 in keyboard, 8 in drives
 */
 
-int findClosestBelowIndexInSortedVector(vector<int> sorted_vector_for_search, int number_to_search){
-	/*
-	This function recives a sorted vector and find the most closest below number to number_to_search. 
-	Then, return the index of that value.
-	If the vector have a number equal to number_to_search, return the index of that number.
-	If the vector doesn't have a number close below (all number are higher), return -1
-	*/
+int highestValueUnderRange(vector<int> values_vector, int highest_value, 
+                           int lower_value) {
+    int the_value = 0;
 
-	int min_index = 0;
-	int max_index = sorted_vector_for_search.size() - 1; 
+    for(int i = values_vector.size() - 1; i >= 0; i--) {
+        if(values_vector[i] >= lower_value and 
+           values_vector[i] <= highest_value ) {
 
-	while(min_index != max_index){
-		int middle_index = floor(min_index + max_index);
-		int middle_value = sorted_vector_for_search[middle_index];
+            the_value = values_vector[i];
+            break;
+        }
+    }
 
-		if(middle_value == number_to_search){
-			return middle_index;
-		}
-		else if(middle_value < number_to_search){
-			min_index = middle_index + 1;
-		}
-		else if(middle_value > number_to_search){
-			max_index = max_index - 1;
-		}
-	}
-
-	if (sorted_vector_for_search[min_index] > number_to_search){
-		return -1;
-	}
-
-	return max_index;
+    return the_value;
 }
 
 int getMoneySpent(vector<int> keyboards, vector<int> drives, int b) {
-	vector<int> keyboards_sorted = keyboards;
-	vector<int> drives_sorted = drives;
-	sort(keyboards_sorted.begin(), keyboards_sorted.end() );
-	sort(drives_sorted.begin(), drives_sorted.end() );
+    int highest_for_expend = 0;
+    sort(keyboards.begin(), keyboards.end() );
+    sort(drives.begin(), drives.end() );
 
-	int keyb_max_price_lower_than = b - drives_sorted[0];
-	int keyb_max_price_index = findClosestBelowIndexInSortedVector(keyboards_sorted, keyb_max_price_lower_than);
-	if (keyb_max_price_index == -1){
-		return -1;
-	}
+    int least_expensive_keyboard = keyboards[0];
+    int least_expensive_drive = drives[0];
 
-	int drives_max_price_lower_than = b - keyboards_sorted[keyb_max_price_index];
-	int drives_max_price_index = findClosestBelowIndexInSortedVector(drives_sorted, drives_max_price_lower_than);
-	if (drives_max_price_index == -1){
-		return -1;
-	}
+    int most_expensive_keyboard_possible = highestValueUnderRange(keyboards, b - least_expensive_drive, least_expensive_drive);
+    int most_expensive_drive_possible = highestValueUnderRange(drives, b - least_expensive_keyboard, least_expensive_keyboard);
 
-	int most_expensive_purchase = keyboards_sorted[keyb_max_price_index] 
-								  + drives_sorted[drives_max_price_index];
-	
-	return most_expensive_purchase;
+    cout << "final values: " << endl;
+    cout << least_expensive_keyboard << " + "<< most_expensive_drive_possible << endl;
+    cout << least_expensive_drive << " + " << most_expensive_keyboard_possible << endl;
+    if(most_expensive_drive_possible == 0 and most_expensive_keyboard_possible == 0){
+        highest_for_expend = -1;
+    }
+    else{
+        int one_try = least_expensive_drive + most_expensive_keyboard_possible;
+        int second_try = least_expensive_keyboard + most_expensive_drive_possible;
+
+        highest_for_expend = (one_try > second_try) ? (one_try) : (second_try);
+    }
+
+    return highest_for_expend;
 }
